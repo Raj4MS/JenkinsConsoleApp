67806d86c46c345e0a46d3372877c10ea7d4cb18
Raj kumar <rajkumar4543@gmail.com>
2018-06-29 17:02:04 +0530

    first commit

---
+++ JenkinsConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JenkinsConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = "raj kumar";
+            Console.WriteLine("My name is "+ name);
+        }
+    }
+}
